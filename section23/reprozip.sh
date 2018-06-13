012c53fb037e2be0d7f3f332630f1a4ead63aa46
Satrajit Ghosh <satrajit.ghosh@gmail.com>
2018-06-13 15:33:47 +0530

    Update reprozip.sh

--- section23/reprozip.sh
+++ section23/reprozip.sh
@@ -1,30 +1,50 @@
 #!/bin/bash
+
+set -e -u
+
+# remember the original directory, just to make some admin parts easier
+# but generally irrelevant
+srcdir=$(pwd)
+wdir=${1:-demo}
+mkdir -p ${wdir}
+cd ${wdir}
+echo "Workdir at `pwd`"
+
 # Now let's switch gears a little bit to record provenance slightly differently
-mkdir reprozip
+datalad create reprozip
 cd reprozip
 
 # Let's get a new dataset
-datalad install ///openfmri/ds000114
+datalad install -d . -s ///openfmri/ds000114
 
 # and retrieve only the T1s from one participant
-datalad get sub-02/ses-*test/anat/sub-02_ses-*test_T1w.nii.gz
+datalad get ds000114/sub-02/ses-*test/anat/sub-02_ses-*test_T1w.nii.gz
 
 # Now we will use a tool called reprozip
 datalad run singularity exec -B /tmp --bind $PWD/. -H $PWD/. \
  ../glm_analysis/.datalad/environments/fsl/image \
  reprozip trace bet \
  ./ds000114/sub-02/ses-test/anat/sub-02_ses-test_T1w.nii.gz brain -m
-singularity exec -B /tmp --bind $PWD/. -H $PWD/. \
+
+datalad run singularity exec -B /tmp --bind $PWD/. -H $PWD/. \
  ../glm_analysis/.datalad/environments/fsl/image reprozip pack
 
 # and now let's look at the information stored
-reprounzip info experiment.rpz
-reprounzip showfiles experiment.rpz
+datalad run singularity exec -B /tmp --bind $PWD/. -H $PWD/. \
+ ../glm_analysis/.datalad/environments/fsl/image reprounzip info experiment.rpz
+
+datalad run singularity exec -B /tmp --bind $PWD/. -H $PWD/. \
+ ../glm_analysis/.datalad/environments/fsl/image reprounzip showfiles experiment.rpz
+
+conda activate section3
 
 # reprozip can be used to rerun the analysis in a different place
 reprounzip docker setup experiment.rpz repeat
 
-cd ../glm-analysis
 
 # how would we use reprozip to capture the details of the glm
-datalad run singularity exec -B /tmp --bind /nobackup/scratch/Fri/satra/training/test-exercises/demo/glm_analysis/. -H /nobackup/scratch/Fri/satra/training/test-exercises/demo/glm_analysis/. .datalad/environments/fsl/image reprozip trace feat 'sub-02/1stlvl_design.fsf'
+conda activate section2
+
+cd ../glm-analysis
+datalad run singularity exec -B /tmp --bind $PWD/. -H $PWD/. \
+  .datalad/environments/fsl/image reprozip trace feat 'sub-02/1stlvl_design.fsf'
